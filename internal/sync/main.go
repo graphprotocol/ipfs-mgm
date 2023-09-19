ae5042747288fa87073d82aa9bbaeb97564062ac
Ruslan Rotaru <rotarur.social.apps@gmail.com>
2023-09-19 08:20:42 +0100

    fixed IPFS list endpoint added IPFS add endpoint changed the code to use more abstract functions

--- internal/sync/main.go
+++ internal/sync/main.go
@@ -2,8 +2,12 @@ package sync
 
 import (
 	"fmt"
-	"github.com/spf13/cobra"
+	"encoding/json"
+	// "strings"
+	"time"
+
 	"github.com/graphprotocol/ipfs-mgm/internal/utils"
+	"github.com/spf13/cobra"
 )
 
 var SyncCmd = &cobra.Command{
@@ -20,19 +24,80 @@ func init() {
 	SyncCmd.MarkFlagRequired("source")
 	SyncCmd.Flags().StringP("destination", "d", "", "IPFS destination endpoint")
 	SyncCmd.MarkFlagRequired("destination")
+	SyncCmd.Flags().StringP("from-file", "f", "", "Sync CID's from file. Has priority over <source> and <destination> flags")
 }
 
 func Sync(cmd *cobra.Command) {
+	timeStart := time.Now()
+
+
 	src, err := cmd.Flags().GetString("source")
 	if err != nil {
 		fmt.Println(err)
 	}
+
+	// fromFile, err := cmd.Flags().GetString("from-file")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+
+	// if len(fromFile) != 0 {
+	// 	syncFromFile(src, fromFile)
+	// }
 
 	dst, err := cmd.Flags().GetString("destination")
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	fmt.Println(dst)
-	utils.IPFSPost(src)
+	fmt.Printf("Syncing from %s to %s\n", src, dst)
+
+	// Create the API URL for the IPFS pin/ls operation
+	apiURL := src + utils.IPFS_LIST_ENDPOINT
+
+	// Get the HTTP response
+	res := utils.PostIPFS(apiURL, nil)
+
+	// Get the HTTP body
+	body, _ := utils.GetHTTPBody(res)
+
+	m := make(map[string]interface{})
+	err = json.Unmarshal(body, &m)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(m)
+
+	// newBody := strings.Split(string(body), "\n")
+	//
+	// for _, cid := range newBody {
+	// 	fmt.Printf("CID: %v\n", cid)
+	// 	fmt.Println(reflect.TypeOf(cid))
+	// }
+
+	// Print Final statistics
+	fmt.Printf("Total number of objects: %d\n", len(body))
+	fmt.Printf("Total time: %s\n", time.Since(timeStart))
 }
+
+// func syncFromFile(url string, f string) {
+// 	fmt.Println(f)
+//
+// 	// Create the API URL for the IPFS GET request
+// 	apiUrl := url + utils.IPFS_CAT_ENDPOINT
+//
+// 	f, err := os.Open(f)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+//
+// 	// Make GET HTTP request
+// 	res := utils.GetIPFS(apiUrl)
+//
+// 	//Get the HTTP body
+// 	body, _ := utils.GetHTTPBody(res)
+//
+// 	fmt.Println(string(body))
+// 	os.Exit(0)
+// }
--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -5,15 +5,19 @@ import (
     "io"
     "io/ioutil"
     "strings"
-    "time"
-
     "os"
 
     "net/http"
 )
 
-var IPFS_LIST_ENDPOINT string = "/ipfs/api/v0/pin/ls?stream-channels=true"
+var IPFS_LIST_ENDPOINT string = "/ipfs/api/v0/pin/ls?stream=true"
+var IPFS_CAT_ENDPOINT string = "/ipfs/api/v0/cat?arg="
 var HEADER_APP_JSON string = "application/json"
+
+type IPFSStruct struct {
+    Cid string `json:"cid"`
+    Type string `json:"type"`
+}
 
 func ValidateEndpoints (src string, dst string) error {
     if src == dst {
@@ -22,18 +26,20 @@ func ValidateEndpoints (src string, dst string) error {
     return nil
 }
 
-func postListUrl(url string, payload io.Reader) *http.Response {
-    // Create an HTTP client
-    client := &http.Client{
-        Timeout: 10 * time.Minute,
+func GetIPFS(url string) *http.Response {
+    req, err := http.NewRequest(http.MethodGet, url, nil)
+    if err != nil {
+        fmt.Println(err)
+        os.Exit(1)
     }
 
-    // Create the URL for the IPFS LIST
-    apiURL := url + IPFS_LIST_ENDPOINT
+    // Set custom User-Agent for cloudflare WAF policies
+    req.Header.Set("User-Agent", "graphprotocol/ipfs-mgm")
 
-    fmt.Printf("Making request to: %s\n", apiURL)
+    // Create an HTTP client
+    client := &http.Client{}
 
-    res, err := client.Post(apiURL, HEADER_APP_JSON, payload)
+    res, err := client.Do(req)
     if err != nil {
         fmt.Println("Error making API request:", err)
         os.Exit(1)
@@ -44,27 +50,45 @@ func postListUrl(url string, payload io.Reader) *http.Response {
         os.Exit(1)
     }
 
-    fmt.Println(res.Status)
+    return res
+}
 
-    fmt.Println(res)
+func PostIPFS(url string, payload io.Reader) *http.Response {
 
-    defer res.Body.Close()
+    req, err := http.NewRequest(http.MethodPost, url, payload)
+    if err != nil {
+        fmt.Println(err)
+        os.Exit(1)
+    }
+
+    // Set custom User-Agent for cloudflare WAF policies
+    req.Header.Set("User-Agent", "graphprotocol/ipfs-mgm")
+
+    // Create an HTTP client
+    client := &http.Client{}
+
+    res, err := client.Do(req)
+    if err != nil {
+        fmt.Println("Error making API request:", err)
+        os.Exit(1)
+    }
+
+    if s := res.Status; strings.HasPrefix(s, "5") || strings.HasPrefix(s, "4") {
+        fmt.Printf("There was an error with the request. Error code: HTTP %s\n", s)
+        os.Exit(1)
+    }
 
     return res
 }
 
-func IPFSPost(src string) {
-    // Make POST to IPFS list endpoint
-    res := postListUrl(src, nil)
-
+func GetHTTPBody(h *http.Response) ([]byte, error) {
     // Read the body response
-    body, err := ioutil.ReadAll(res.Body)
+    body, err := ioutil.ReadAll(h.Body)
     if err != nil {
         fmt.Println("Error reading response body:", err)
         os.Exit(1)
     }
+    defer h.Body.Close()
 
-    // Print all IPFS content
-    fmt.Println("Files and directories in remote IPFS root:")
-    fmt.Println(string(body))
+    return body, err
 }
